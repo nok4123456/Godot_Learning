2492e976b9d0537c03ed39d79264e20fe6dd8d20
nok4123456 <nok4123456@gmail.com>
2024-06-17 00:32:11 +0800

    create a game camera follow the main character

---
+++ scenes/basic_enemy/basic_enemy.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+const MAX_SPEED = 75
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var target_direction = get_direction_to_player()
+	velocity = target_direction * MAX_SPEED
+	move_and_slide()
+
+
+func get_direction_to_player() -> Vector2:
+	var player_node = get_tree().get_first_node_in_group("player") as Node2D
+	var target_direction = Vector2.ZERO
+	if player_node != null:
+		target_direction = (player_node.global_position - global_position).normalized()
+	return target_direction
